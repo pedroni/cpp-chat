44a161587bcb6d99ee1d874e466abebe36f674e8
pedroni <pedronidegodois@hotmail.com>
2025-12-25 20:13:44 -0300

    bring back main client

--- client/main.cpp
+++ client/main.cpp
@@ -1,101 +1,69 @@
-#include <arpa/inet.h>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <unistd.h>
 
-#define PORT "3490"
-#define MAX_BYTES 100
+#include "chat.h"
+#include <chrono>
+#include <fmt/printf.h>
+#include <iostream>
+#include <ncurses.h>
+#include <thread>
 
-// void * means pointer of something/unknown thing
-void *get_in_addr(struct sockaddr *sa) {
-  // AF_INET stands for IPv4 family
-  if (sa->sa_family == AF_INET) {
-    struct sockaddr_in *sa = ((sockaddr_in *)sa);
-    return &sa->sin_addr;
+using namespace std;
+
+void readInput(Chat &chat) {
+  while (chat.isRunning()) {
+    chat.currentCh = getch();
+
+    switch ((int)chat.currentCh) {
+    case 10:
+      chat.handleInput();
+      chat.input = "";
+      break;
+    default:
+      chat.input += chat.currentCh;
+      break;
+    }
   }
-
-  struct sockaddr_in6 *sa6 = ((sockaddr_in6 *)sa);
-  return &sa6->sin6_addr;
 }
 
-int main(int argc, char *argv[]) {
-  int sockfd, numbytes;
-  char buff[MAX_BYTES];
+void renderChat(const Chat &chat) {
+  constexpr int fps = 240;
+  constexpr int msPerFrame = 1000 / fps;
 
-  struct addrinfo hints, *servinfo, *p;
-  int rv;
-  char humanIpAddress[INET6_ADDRSTRLEN];
+  while (chat.isRunning()) {
+    chat.render();
 
-  if (argc != 2) {
-    printf("usage: ChatClient hostname");
-    exit(1);
+    // fakes running at specific timestep
+    this_thread::sleep_for(chrono::milliseconds(msPerFrame));
   }
+}
 
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
+int main() {
+  // clears the screen and presents a virtual screen
+  initscr();
 
-  rv = getaddrinfo(argv[1], PORT, &hints, &servinfo);
-  if (rv != 0) {
-    printf("failed to getaddrinfo: %s\n", gai_strerror(rv));
-    exit(1);
-  }
+  // with the default c++ behavior everytime we type something on the terminal
+  // using std::cin we would see the output of the keys that are pressed, this
+  // hides whatever key is pressed
+  noecho();
 
-  for (p = servinfo; p != NULL; p = p->ai_next) {
+  // enables special keys to be used with constant KEY_ENTER for example, this
+  // also enables the usage of F1, F2 keys
+  // dunno? didnt work with KEY_ENTER had to use int(10)
+  keypad(stdscr, true);
 
-    sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-    if (sockfd == -1) {
-      perror("failed to socket handshake");
-      continue;
-    }
+  Chat chat;
 
-    if (inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-                  humanIpAddress, sizeof humanIpAddress) == NULL) {
-      perror("failed to inet_ntop");
-      continue;
-    }
+  thread input{readInput, ref(chat)};
+  thread renderer{renderChat, ref(chat)};
 
-    printf("Connecting to %s\n", humanIpAddress);
+  // join in threads works in similar fashion as an await in javascript
+  input.join();
+  renderer.join();
 
-    if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-      perror("failed to connect");
-      close(sockfd);
-      continue;
-    }
+  // ends the program, this goes back to the original terminal output, and frees
+  // the memory allocated by ncurse
+  endwin();
 
-    break;
-  }
+  std::cout << "Sad to see you go!" << std::endl;
 
-  // clears memory of linked list
-  freeaddrinfo(servinfo);
-
-  if (p == NULL) {
-    fprintf(stderr, "could not find addressinfo for hostname %s\n", argv[1]);
-    exit(2);
-  }
-
-  printf("Connected to %s\n", humanIpAddress);
-
-  const char *message = "A message from the client";
-  if (send(sockfd, message, strlen(message), 0) == -1) {
-    printf("Failed to send\n");
-  }
-
-  printf("Now we want to receive it back\n");
-  numbytes = recv(sockfd, buff, MAX_BYTES - 1, 0);
-  if (numbytes == -1) {
-    perror("Failed to recv");
-    exit(1);
-  }
-
-  buff[numbytes] = '\0';
-
-  printf("I received it back: %s\n", buff);
-
-  close(sockfd);
-  exit(0);
+  return 0;
 }
