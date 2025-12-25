595eee6d741082d4b3fdf073f533352d9f4daa32
pedroni <pedronidegodois@hotmail.com>
2025-12-25 20:12:55 -0300

    server code is done?????? lets write the client finally and see what awaits us. i wasnt able to test the server yet

--- server/main.cpp
+++ server/main.cpp
@@ -7,8 +7,10 @@
 #include <fmt/printf.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <poll.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/poll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -19,31 +21,8 @@
 #define BACKLOG 10
 #define MAX_BYTES 100
 
-void sigchld_handler(int s) {
-  (void)s;
+int get_listener() {
 
-  int saved_errno = errno;
-
-  while (waitpid(-1, NULL, WNOHANG) > 0) {
-    // do nothing just wait for the pid (process id?)
-  }
-
-  errno = saved_errno;
-}
-
-// void * means pointer of something/unknown thing
-void *get_in_addr(struct sockaddr *sa) {
-  // AF_INET stands for IPv4 family
-  if (sa->sa_family == AF_INET) {
-    struct sockaddr_in *sa = ((sockaddr_in *)sa);
-    return &sa->sin_addr;
-  }
-
-  struct sockaddr_in6 *sa6 = ((sockaddr_in6 *)sa);
-  return &sa6->sin6_addr;
-}
-
-int main() {
   int sockfd;
 
   struct addrinfo hints, *servinfo, *p;
@@ -61,7 +40,7 @@ int main() {
   int rv;
   if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
     fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-    return 1;
+    return -1;
   }
 
   int yes = 1;
@@ -78,7 +57,7 @@ int main() {
     }
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
       perror("failed to setsockopt");
-      return 1;
+      return -1;
     }
     if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
       close(sockfd);
@@ -95,102 +74,136 @@ int main() {
   // of the loop
   if (p == NULL) {
     printf("Unable to bind to any of the addrinfo in servinfo.");
-    return 1;
+    return -1;
   }
 
   if (listen(sockfd, BACKLOG) == -1) {
     perror("failed to listen");
-    return 1;
+    return -1;
   }
 
-  struct sigaction sa;
-  sa.sa_handler = sigchld_handler;
-  sigemptyset(&sa.sa_mask);
-  sa.sa_flags = SA_RESTART;
-  // assign to "sa"
-  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-    perror("sigaction");
-    return 1;
+  return sockfd;
+}
+
+const char *inet_ntop2(void *addr, char *buf, size_t size) {
+  struct sockaddr_storage *sas = (struct sockaddr_storage *)addr;
+  struct sockaddr_in *sa4;
+  struct sockaddr_in6 *sa6;
+  void *src;
+
+  switch (sas->ss_family) {
+  case AF_INET:
+    sa4 = (struct sockaddr_in *)addr;
+    src = &(sa4->sin_addr);
+    break;
+  case AF_INET6:
+    sa6 = (struct sockaddr_in6 *)addr;
+    src = &(sa6->sin6_addr);
+    break;
+  default:
+    return NULL;
   }
 
-  printf("Waiting for connections...\n");
+  return inet_ntop(sas->ss_family, src, buf, size);
+}
 
-  int newfd;
-  struct sockaddr_storage their_addr;
-  socklen_t sin_size;
-  char s[INET6_ADDRSTRLEN];
+void remove_poll(std::vector<pollfd> &pollfds, unsigned long &i) {
+  printf("removing connection %d\n", pollfds[i].fd);
 
-  std::vector<int> clientsfd;
-  std::vector<char> messages;
+  close(pollfds[i].fd);
 
-  // main connect loop, wait for connections?
-  while (1) {
-    sin_size = sizeof their_addr;
-    newfd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-    if (newfd == -1) {
-      perror("failed to accept");
+  pollfds[i] = pollfds.back();
+  pollfds.pop_back();
+  i -= 1;
+}
+
+int new_poll(std::vector<pollfd> &pollfds, int sockfd) {
+  struct sockaddr_storage remoteaddr; // Client address
+  char remoteIP[INET6_ADDRSTRLEN];
+
+  socklen_t addrlen = sizeof remoteaddr;
+
+  // Newly accept()ed socket descriptor
+  int newfd = accept(sockfd, (struct sockaddr *)&remoteaddr, &addrlen);
+
+  if (newfd == -1) {
+    perror("accept");
+    return -1;
+  }
+
+  printf("pollserver: new connection from %s on socket %d\n",
+         inet_ntop2(&remoteaddr, remoteIP, sizeof remoteIP), newfd);
+
+  pollfds.push_back({newfd, POLLIN | POLLOUT, 0});
+  return newfd;
+}
+
+/**
+ * sends the received message to everyone
+ */
+void broadcast_message(int sockfd, std::vector<pollfd> &pollfds,
+                       unsigned long &i) {
+  char buf[256];
+  int numbytes = recv(pollfds[1].fd, buf, sizeof(buf), 0);
+
+  if (numbytes == -1) {
+    perror("failed to receive: ");
+    remove_poll(pollfds, i);
+    return;
+  }
+
+  for (pollfd poll : pollfds) {
+    // avoid sending to the same client and to ourselves
+    if (poll.fd == sockfd || poll.fd == pollfds[i].fd) {
       continue;
     }
 
-    inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr),
-              s, sizeof s);
-    printf("server: got connection from %s\n", s);
-
-    clientsfd.push_back(newfd);
-
-    // fork returns 0 when we succesfully create a child process.
-    // it's also 0 when we're in the child process
-    // for the parent it will have returned the process id (pid)
-    // on error returns -1
-    if (!fork()) {
-      // in here we're the child process
-      // close(sockfd); // child doesn't need the listener, it will only send
-      // events from now on
-      unsigned int count = 0;
-      unsigned int numbytes = 0;
-      char buff[MAX_BYTES];
-
-      while (1) {
-        count++;
-
-        numbytes = recv(newfd, buff, MAX_BYTES - 1, 0);
-        if (numbytes == -1) {
-          perror("Failed to recv");
-          exit(1);
-        }
-
-        if (numbytes > 0) {
-          buff[numbytes] = '\0';
-          printf("Received message: %s\n Forwarding...\n", buff);
-          messages.push_back(buff[numbytes]);
-        }
-      }
-
-      printf("closing connection");
-      close(newfd); // close child
-      exit(0); // exit process with empty 0, successfull!, this only exits the
-               // child process? thats odd. but i guess its alright. so what
-               // happens if we return 0? i think we wont stop the main process
+    if (send(poll.fd, buf, numbytes, 0) == -1) {
+      perror("failed to send: ");
+      remove_poll(pollfds, i);
+      continue;
     } else {
-      while (1) {
-        if (messages.size() > 0) {
-          printf("we have messages to send over\n");
-          for (int clientfd : clientsfd) {
-            char *message = &messages.at(messages.size() - 1);
-            if (send(clientfd, message, strlen(message), 0) == -1) {
-              perror("Failed to send");
-            }
-            messages.pop_back();
-          }
-          // messages.at(0);
-        }
-      }
-
-      // in here we're on the parent process
-      // close(newfd); // the parent doesnt care about the child anymore
-      // maybe we do care when sending from the server to multiple clients.
+      printf("sent.\n");
     }
   }
+}
+
+int main() {
+
+  int sockfd = get_listener();
+  if (sockfd == -1) {
+    printf("Something went wrong. Not able to get a listener.\n");
+    return -1;
+  }
+
+  std::vector<pollfd> pollfds;
+  pollfds.reserve(10);
+
+  pollfds.push_back({sockfd, POLLIN, 0});
+
+  printf("Waiting for connections...\n");
+
+  // main connect loop, wait for connections?
+  while (1) {
+    int poll_count =
+        poll(pollfds.data(), pollfds.size(), 100); // timeout in 100ms
+
+    for (size_t i = 0; i < pollfds.size(); i++) {
+      if (!(pollfds[i].revents & (POLLIN | POLLHUP))) {
+        continue;
+      }
+
+      // when its the socket we wait for new connections
+      if (pollfds[i].fd == sockfd) {
+        new_poll(pollfds, sockfd);
+      } else {
+        // if not the socket, then its a client here we're able to send or recv
+        broadcast_message(sockfd, pollfds, i);
+      }
+    }
+  }
+
+  printf("Server is closing.");
 
   return 0;
 }
